48abe97a7825c91a9b79ae37ed422d4743b9d12a
italiancode <italianloader@gmail.com>
2025-02-24 17:16:30 +0100

    Something just got updated!

--- src/component/layout/Layout.tsx
+++ src/component/layout/Layout.tsx
@@ -27,7 +27,7 @@ export function Layout({ children }: LayoutProps) {
           <NetworkSwitchNotification className="container mx-auto px-4 sm:px-6 lg:px-8" />
 
           {/* Main Content */}
-          <main className="container mx-auto w-full max-w-md px-4 py-4 sm:px-6 lg:px-8 lg:py-12 flex-grow">
+          <main className="container mx-auto w-full max-w-md px-4 py-6 sm:px-6 lg:px-8 lg:py-12 flex-grow">
             {children}
           </main>
 
