0e390d1a80de391e0a705c5f351fb65ec5ed3a51
Big Dreams Web3 - Crafts ◯/| <123673781+italiancode@users.noreply.github.com>
2025-03-01 12:12:54 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ import type { Metadata } from "next";
 // import { isTokenAccepted } from "@/interact/TokenCheck";
 
 export const metadata: Metadata = {
-  title: "ChainPay Africa - Home",
+  title: "ChainPay Africa - Pay Bills with Crypto",
   description:
-    "Welcome to ChainPay Africa, your go-to app for seamless bill payments.",
+    "ChainPay Africa is a blockchain-powered payment platform that lets you pay for airtime, data, electricity, and other services using USDC or BNB on Binance Smart Chain. Enjoy fast, secure, and borderless transactions.",
 };
 
 export default function Home() {
