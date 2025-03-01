dbcb3a03617b487b92ac25823d4dbbe527fd6b97
Big Dreams Web3 - Crafts ◯/| <123673781+italiancode@users.noreply.github.com>
2025-03-01 12:20:11 +0100

    Update index.ts

--- src/utils/web3/chains/index.ts
+++ src/utils/web3/chains/index.ts
@@ -37,31 +37,31 @@ export const crossfiTestnet = defineChain({
       image: "https://example.com/pNGN.png",
     },
 
-    xUSD: {
+    XUSD: {
       id: "0x0000000000000000000000000000000000000002",
       name: "XUSD",
       symbol: "xUSD",
       decimals: 18,
-      contractAddress: "0xea26a662333a2a5E87fB6851fc24a47fa53d98D1",
+      contractAddress: "0x12e048d4f26f54c0625ef34fabd365e4f925f2ff",
       image: "https://crossfi.org/wp-content/uploads/2024/12/xusd-2.svg",
     },
 
-    xUSDT: {
-      id: "0x0000000000000000000000000000000000000003",
-      name: "XUSDT",
-      symbol: "XUSDT",
-      decimals: 18,
-      contractAddress: "0x0000000000000000000000000000000000000000",
-      image: "https://example.com/USDT.png",
-    },
+    // xUSDT: {
+    //   id: "0x0000000000000000000000000000000000000003",
+    //   name: "XUSDT",
+    //   symbol: "XUSDT",
+    //   decimals: 18,
+    //   contractAddress: "0x0000000000000000000000000000000000000000",
+    //   image: "https://example.com/USDT.png",
+    // },
 
-    xUSDC: {
-      id: "0x0000000000000000000000000000000000000004",
-      name: "XUSDC",
-      symbol: "XUSDC",
-      decimals: 18,
-      contractAddress: "0x0000000000000000000000000000000000000000",
-      image: "https://example.com/USDC.png",
-    },
+    // xUSDC: {
+    //   id: "0x0000000000000000000000000000000000000004",
+    //   name: "XUSDC",
+    //   symbol: "XUSDC",
+    //   decimals: 18,
+    //   contractAddress: "0x0000000000000000000000000000000000000000",
+    //   image: "https://example.com/USDC.png",
+    // },
   } as Record<string, PaymentToken>,
 });
