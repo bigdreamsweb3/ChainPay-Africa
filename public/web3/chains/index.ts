a07ceed0e289172e1d59280cce7911dd0fc12583
italiancode <italianloader@gmail.com>
2025-04-05 09:50:43 -0700

    Something just got updated!

--- public/web3/chains/index.ts
+++
@@ -1,7 +0,0 @@
-import { baseSepolia } from "./testnet/BaseChain";
-import { monadTestnet } from "./testnet/MonadChain";
-import { ChainPayChain } from "../types";
-
-export const acceptedChains = [baseSepolia, monadTestnet] as ChainPayChain[];
-
-export { baseSepolia, monadTestnet };
